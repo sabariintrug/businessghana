b817a7627a4195602285cf8b315f34a8556dd10d
sabari <sabari@intrug.com>
2012-09-07 12:39:00 +0530

    changes........made

--- index.php
+++ index.php
@@ -8,4 +8,4 @@
 	header('Location: '.$uri.'/xampp/');
 	exit;
 ?>
-Something is wrong with the XAMPP installation :-(yesasdasdasddgdsfgdfsgdsf	dfgdfgdf
+Something is wrong with the XAMPP installation :-(
